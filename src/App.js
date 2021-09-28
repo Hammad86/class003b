c0a646a1478eb0fb6f5e01e5013470a8fe7b830f
Hammad86 <sunnyhammad1998@gmail.com>
2021-09-28 18:40:15 +0500

    made component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import Counter from './Components/Counter';
+
 
 function App() {
+  let [day , setDay] = useState(true)
   return (
-    <div className="App">
+    <div className={` ${day ? 'day1' : 'night' }`}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>{day} Time</h1>
+        <Counter/>
+        <button onClick={()=>{setDay(!day) }}> Change </button>
+        {console.log(day)}
       </header>
     </div>
   );
---
+++ src/Components/Counter.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+function Counter() {
+    let [count, setCount] = useState(0)
+    
+    return (
+        <div >
+            
+            <h2>Counter Value is : {count} </h2>
+            <button onClick={()=>{setCount(++count)}}>Increment</button>
+            
+            <button onClick={()=>{setCount(--count)}}>Decrement</button>
+        </div>
+    )
+}
+
+export default Counter
